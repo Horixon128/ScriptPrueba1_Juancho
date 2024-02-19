53b4318f41ed4dca0b003e86415169ed4d8bbc48
avatarclon <DaviidRodriguez777@gmail.com>
2024-02-19 15:24:02 -0500

    Verificar configuracion ip del Host

--- Script1.sh
+++ Script1.sh
@@ -9,4 +9,9 @@ echo "Save information script" >  Automatizacion.txt
 command_to_run="service --status-all"
 $command_to_run
 
+# Verificar configuracion ip del host
+
+ip_address=$(ipconfig | grep "IPv4 Address" | awk '{ print $2 }' | head -n 1)
+echo "La dirección IP de Windows es: $ip_address"
+
 
