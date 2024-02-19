d7f44e9a3188dbb7d158bcdd8c67527be97e2414
avatarclon <DaviidRodriguez777@gmail.com>
2024-02-19 14:53:14 -0500

    Verificar Servicios

--- Script1.sh
+++ Script1.sh
@@ -4,4 +4,9 @@
 
 echo "Save information script" >  Automatizacion.txt 
 
+# Ejecucion de comando para revisar que servicios estan activos
+
+command_to_run="service --status-all"
+$command_to_run
+
 
